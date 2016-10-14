3432ee9bdf9650cd2f93d0d6a8d23e491ce37ab0
mrprvt <mrprvt@protonmail.com>
2016-10-14 13:00:03 +0200

    spoj-liczby-pierwsze
    System sprawdza warunki dla pojedynczej liczby.

---
+++ main.cpp
@@ -0,0 +1,102 @@
+/* PROBLEM
+
+Sprawdź, które spośród danych liczb są liczbami pierwszymi
+Input
+
+n - liczba testów n<100000, w kolejnych liniach n liczb z przedziału [1..10000]
+Output
+
+Dla każdej liczby słowo TAK, jeśli liczba ta jest pierwsza, słowo: NIE, w przeciwnym wypadku.
+Example
+
+Input:
+3
+11
+1
+4
+
+Output:
+TAK
+NIE
+NIE
+
+*/
+
+/* ROZWIĄZANIE
+
+    pisz "TAK" gdy:
+
+    - jest dodatnia
+    - ma więcej niż 2 dzielniki (reszta z dzielenia = 0)
+
+    - działanie: podziel n przez wszystkie liczby całkowite większe od 1, a mniejsze od n
+
+*/
+
+#include <iostream>
+
+    using namespace std;
+
+int main(){
+
+    int liczba;
+    int liczbaKolejek;
+    int liczbaDzielnikow;
+
+    //liczbaKolejek = 100;
+
+    cin >> liczbaKolejek;
+
+    // wartosc liczby to ilosc razy
+
+    // liczba-1 ponieważ nie potrzeba dzielic przez wartosc podanej liczby
+
+    for(int i = 0; i < liczbaKolejek; i++){
+
+    liczbaDzielnikow = 0;
+
+        cin >> liczba;
+
+        for(int i = 1; i <= liczba; i++){
+
+            if(liczba > 1 && liczba%2 > 0){
+
+                if(liczba%i == 0) liczbaDzielnikow++;
+
+            }else liczbaDzielnikow = 3;
+
+            if(liczba%i == 0 && liczba > 2) cout << "Dzielniki liczby " << liczba << " to " << i << endl;
+
+        }
+
+        if(liczbaDzielnikow <= 2) cout << "TAK\n"; // jest liczba pierwsza
+        else cout << "NIE\n"; // nie jest liczba pierwsza
+
+    }
+
+        return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
